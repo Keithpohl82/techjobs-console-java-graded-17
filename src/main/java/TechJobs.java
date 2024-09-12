dcf404285b1a34a59e43ec639b461847ea490fb8
keithpohl82 <keith.pohl82@gmail.com>
2024-09-12 18:52:19 -0500

    2 test passed printjobs/noresults

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,28 +112,28 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if(someJobs.isEmpty()){
-            System.out.println("No Results");
-        }
-        System.out.println("");
-        for(int i = 0; i < someJobs.size(); i++) {
-            HashMap<String, String> job = someJobs.get(i);
-            System.out.println("*****");
-            for (Map.Entry<String, String> entry : job.entrySet()) {
-                String jobKey = entry.getKey();
-                String jobValue = entry.getValue();
 
-                System.out.println("" + jobKey + ": " + jobValue);
-            }
-            System.out.println("*****");
-
-        }
-    }
-}
+        if (someJobs.isEmpty()) {
+            System.out.print("No Results");
+        } else {
+            System.out.println("");
+            for (int i = 0; i < someJobs.size(); i++) {
+                HashMap<String, String> job = someJobs.get(i);
+                System.out.println("*****");
+                for (Map.Entry<String, String> entry : job.entrySet()) {
+                    String jobKey = entry.getKey();
+                    String jobValue = entry.getValue();
+                    System.out.println("" + jobKey + ": " + jobValue);
+                }
+                System.out.println("*****");
+            };
+        };
+    };
+};
